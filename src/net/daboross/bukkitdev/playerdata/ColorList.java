297ae608e7208a0df3b3d7b967a08a18120200db
daboross <dabodialga@gmail.com>
2013-01-23 12:33:46 -0800

    Changed Colors

--- src/net/daboross/bukkitdev/playerdata/ColorList.java
+++ src/net/daboross/bukkitdev/playerdata/ColorList.java
@@ -6,26 +6,26 @@ import org.bukkit.ChatColor;
  *
  * @author daboross
  */
-public class ColorList {
+public final class ColorList {
 
-    public static String MAIN = ChatColor.AQUA.toString();
-    public static String NAME = ChatColor.GRAY.toString();
-    public static String NUMBER = ChatColor.RED.toString();
-    public static String CMD = ChatColor.GREEN.toString();
-    public static String HELP = ChatColor.DARK_AQUA.toString();
-    public static String SUBCMD = ChatColor.AQUA.toString();
-    public static String NOPERM = ChatColor.RED.toString();
-    public static String ARGS = ChatColor.BLUE.toString();
-    public static String ILLEGALARGUMENT = ChatColor.AQUA.toString();
-    public static String ERROR = ChatColor.DARK_RED.toString();
-    public static String ERROR_ARGS = ChatColor.RED.toString();
-    public static String SERVERNAME = ChatColor.BLUE.toString();
-    public static String DATA_HANDLE_SLASH = ChatColor.GRAY.toString();
-    public static String DIVIDER = ChatColor.GRAY.toString();
-    public static String MAIN_DARK = ChatColor.DARK_GREEN.toString();
-    public static String BROADCAST = ChatColor.RED.toString();
+    public static final String MAIN = ChatColor.AQUA.toString();
+    public static final String NAME = ChatColor.GRAY.toString();
+    public static final String NUMBER = ChatColor.RED.toString();
+    public static final String CMD = ChatColor.GREEN.toString();
+    public static final String HELP = ChatColor.DARK_AQUA.toString();
+    public static final String SUBCMD = ChatColor.AQUA.toString();
+    public static final String NOPERM = ChatColor.RED.toString();
+    public static final String ARGS = ChatColor.BLUE.toString();
+    public static final String ILLEGALARGUMENT = ChatColor.AQUA.toString();
+    public static final String ERROR = ChatColor.DARK_RED.toString();
+    public static final String ERROR_ARGS = ChatColor.RED.toString();
+    public static final String SERVERNAME = ChatColor.BLUE.toString();
+    public static final String DATA_HANDLE_SLASH = ChatColor.GRAY.toString();
+    public static final String DIVIDER = ChatColor.GRAY.toString();
+    public static final String MAIN_DARK = ChatColor.DARK_GREEN.toString();
+    public static final String BROADCAST = MAIN;
 
     public static String getBroadcastName(String name) {
-        return ChatColor.DARK_BLUE + "[" + ChatColor.GRAY + name + ChatColor.DARK_BLUE + "]";
+        return ChatColor.DARK_GRAY + "[" + ChatColor.RED + name + ChatColor.DARK_GRAY + "]";
     }
 }
