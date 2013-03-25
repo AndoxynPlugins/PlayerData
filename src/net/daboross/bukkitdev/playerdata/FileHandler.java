da8d38bfded2347208be6b81d4b7ab0386af594d
Dabo Ross <dabodialga@gmail.com>
2013-03-24 21:56:19 -0700

    Removed Unused Import

--- src/net/daboross/bukkitdev/playerdata/FileHandler.java
+++ src/net/daboross/bukkitdev/playerdata/FileHandler.java
@@ -9,7 +9,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 
 /**
