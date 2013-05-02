36b5b0f5f50b13f6c5fc6c73fe8cca3cdfd91e8c
Dabo Ross <dabodialga@gmail.com>
2013-05-01 21:00:43 -0700

    Created new Class to handle custom Metrics reports

---
+++ src/net/daboross/bukkitdev/playerdata/PlayerDataCustomMetrics.java
@@ -0,0 +1,14 @@
+
+package net.daboross.bukkitdev.playerdata;
+
+import net.daboross.bukkitdev.playerdata.metrics.Metrics;
+
+/**
+ *
+ * @author daboross
+ */
+public class PlayerDataCustomMetrics {
+
+    public PlayerDataCustomMetrics(PlayerData playerData, Metrics metrics) {
+    }
+}
